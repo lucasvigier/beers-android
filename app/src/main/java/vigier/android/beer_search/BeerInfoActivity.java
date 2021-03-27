d33d7032dcbbd830d73f4e06e84e0f6f9ab2466a
lucas-vigier <lucas.vigier@etu.univ-nantes.fr>
2021-03-27 21:29:16 +0100

    Intent id works

--- app/src/main/java/vigier/android/beer_search/BeerInfoActivity.java
+++ app/src/main/java/vigier/android/beer_search/BeerInfoActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,10 +16,10 @@ import org.json.JSONException;
 
 public class BeerInfoActivity extends AppCompatActivity {
 
-    TextView textviewBeerInfo ;
+    TextView textViewBeerInfo;
     ImageView imageViewBeer;
-    TextView textviewBeerAttributes ;
-    TextView textViewBeerDesc ;
+    TextView textViewBeerAttributes;
+    TextView textViewBeerDesc;
 
 
     @Override
@@ -26,31 +27,35 @@ public class BeerInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beer_info);
 
-        textviewBeerInfo = findViewById(R.id.textViewBeerInfo);
-        textviewBeerAttributes = findViewById(R.id.textViewBeerAttributes);
+        textViewBeerInfo = findViewById(R.id.textViewBeerInfo);
+        textViewBeerAttributes = findViewById(R.id.textViewBeerAttributes);
         textViewBeerDesc = findViewById(R.id.textViewBeerDesc);
         imageViewBeer = findViewById(R.id.imageViewBeer);
-/*
-        Ion.with(v.getContext())
-                .load("https://api.punkapi.com/v2/beers?beer_name=" + searchText.getText() + "&per_page=5")
-                .setLogging("ION_LOGS", Log.DEBUG)
-                .asString()
-                .setCallback(new FutureCallback<String>() {
-                    @Override
-                    public void onCompleted(Exception e, String result) {
-                        try {
-                            JSONArray beerJsonObject = new JSONArray(result);
-                            for (int i = 0; i < beerJsonObject.length(); i++) {
-                                beersList.add(beerJsonObject.getJSONObject(i).getString("name"));
-                                beersidList.add(beerJsonObject.getJSONObject(i).getInt("id"));
-                            }
-                            listView.setAdapter(beersAdapter);
-                        } catch (JSONException jsonException) {
-                            jsonException.printStackTrace();
-                        }
 
-                    }
-                });*/
+        Bundle extras = getIntent().getExtras();
+
+        int id = extras.getInt("id");
+
+//        Ion.with(Vien)
+//                .load("https://api.punkapi.com/v2/beers?beer_name=" + searchText.getText() + "&per_page=5")
+//                .setLogging("ION_LOGS", Log.DEBUG)
+//                .asString()
+//                .setCallback(new FutureCallback<String>() {
+//                    @Override
+//                    public void onCompleted(Exception e, String result) {
+//                        try {
+//                            JSONArray beerJsonObject = new JSONArray(result);
+//                            for (int i = 0; i < beerJsonObject.length(); i++) {
+//                                beersList.add(beerJsonObject.getJSONObject(i).getString("name"));
+//                                beersidList.add(beerJsonObject.getJSONObject(i).getInt("id"));
+//                            }
+//                            listView.setAdapter(beersAdapter);
+//                        } catch (JSONException jsonException) {
+//                            jsonException.printStackTrace();
+//                        }
+//
+//                    }
+//                });
 
     }
 }
