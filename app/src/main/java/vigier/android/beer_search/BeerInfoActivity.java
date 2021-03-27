0dda29b189d88700b5196f65981a0a3a23cb0390
lucas-vigier <lucas.vigier@etu.univ-nantes.fr>
2021-03-27 22:45:39 +0100

    final

--- app/src/main/java/vigier/android/beer_search/BeerInfoActivity.java
+++ app/src/main/java/vigier/android/beer_search/BeerInfoActivity.java
@@ -16,30 +16,47 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class BeerInfoActivity extends AppCompatActivity {
+    ImageView imageViewBeer;
 
     TextView textViewBeerName;
-    ImageView imageViewBeer;
-    TextView textViewBeerAttributeFirstBrew;
     TextView textViewBeerDesc;
+
+    TextView textViewBeerAttributeFirstBrew;
     TextView textViewBeerAttributesSRM;
     TextView textViewBeerAttributesPH;
     TextView textViewBeerAttributesABV;
     TextView textViewBeerAttributesSubTitle;
 
+    TextView textViewBeerAttributesSRMTitle;
+    TextView textViewBeerAttributesPHTitle;
+    TextView textViewBeerAttributesABVTitle;
+    TextView textViewBeerAttributesFirstBrewTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beer_info);
 
-        textViewBeerName = findViewById(R.id.textViewBeerName);
-        textViewBeerAttributeFirstBrew = findViewById(R.id.textViewBeerAttributeFirstBrew);
-        textViewBeerDesc = findViewById(R.id.textViewBeerDesc);
         imageViewBeer = findViewById(R.id.imageViewBeer);
+
+        textViewBeerName = findViewById(R.id.textViewBeerName);
+        textViewBeerDesc = findViewById(R.id.textViewBeerDesc);
+
+        textViewBeerAttributeFirstBrew = findViewById(R.id.textViewBeerAttributeFirstBrew);
         textViewBeerAttributesSRM = findViewById(R.id.textViewBeerAttributesSRM);
         textViewBeerAttributesPH = findViewById(R.id.textViewBeerAttributesPH);
         textViewBeerAttributesABV = findViewById(R.id.textViewBeerAttributesABV);
         textViewBeerAttributesSubTitle = findViewById(R.id.textViewBeerAttributesSubTitle);
+
+        textViewBeerAttributesSRMTitle = (TextView) findViewById(R.id.textViewBeerAttributesSRMTitle);
+        textViewBeerAttributesPHTitle = (TextView) findViewById(R.id.textViewBeerAttributesPHTitle);
+        textViewBeerAttributesABVTitle = (TextView) findViewById(R.id.textViewBeerAttributesABVTitle);
+        textViewBeerAttributesFirstBrewTitle = (TextView) findViewById(R.id.textViewBeerAttributeFirstBrewTitle);
+
+        textViewBeerAttributesFirstBrewTitle.setText("First Brew");
+        textViewBeerAttributesSRMTitle.setText("SRM");
+        textViewBeerAttributesPHTitle.setText("PH");
+        textViewBeerAttributesABVTitle.setText("ABV");
 
         Bundle extras = getIntent().getExtras();
 
@@ -55,20 +72,21 @@ public class BeerInfoActivity extends AppCompatActivity {
                     public void onCompleted(Exception e, String result) {
                         try {
                             JSONArray beerJsonObject = new JSONArray(result);
+                            Picasso.get().load(beerJsonObject.getJSONObject(0).getString("image_url")).into(imageViewBeer);
+
                             textViewBeerDesc.setText(beerJsonObject.getJSONObject(0).getString("description"));
                             textViewBeerName.setText(beerJsonObject.getJSONObject(0).getString("name"));
-                            Picasso.get().load(beerJsonObject.getJSONObject(0).getString("image_url")).into(imageViewBeer);
+
                             textViewBeerAttributesSRM.setText(beerJsonObject.getJSONObject(0).getString("srm"));
                             textViewBeerAttributesPH.setText(beerJsonObject.getJSONObject(0).getString("ph"));
                             textViewBeerAttributesABV.setText(beerJsonObject.getJSONObject(0).getString("abv"));
                             textViewBeerAttributesSubTitle.setText(beerJsonObject.getJSONObject(0).getString("tagline"));
                             textViewBeerAttributeFirstBrew.setText(beerJsonObject.getJSONObject(0).getString("first_brewed"));
 
+
                         } catch (JSONException jsonException) {
                             jsonException.printStackTrace();
                         }
-
-
                     }
                 });
 
