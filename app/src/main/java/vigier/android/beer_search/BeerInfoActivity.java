70cf9bf6f099ac437acf5c4f68b14891b6a9fc56
E205827A <gwendal.faure@etu.univ-nantes.fr>
2021-03-27 21:20:34 +0100

    beersidList

--- app/src/main/java/vigier/android/beer_search/BeerInfoActivity.java
+++ app/src/main/java/vigier/android/beer_search/BeerInfoActivity.java
@@ -3,24 +3,54 @@ package vigier.android.beer_search;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
 public class BeerInfoActivity extends AppCompatActivity {
 
-    TextView textViewBeerInfoTitle;
-    TextView textViewBeerAttributes;
-    TextView textViewBeerDesc;
+    TextView textviewBeerInfo ;
     ImageView imageViewBeer;
+    TextView textviewBeerAttributes ;
+    TextView textViewBeerDesc ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beer_info);
 
-        textViewBeerInfoTitle = findViewById(R.id.textViewBeerInfo);
-        textViewBeerAttributes = findViewById(R.id.textViewBeerAttributes);
+        textviewBeerInfo = findViewById(R.id.textViewBeerInfo);
+        textviewBeerAttributes = findViewById(R.id.textViewBeerAttributes);
         textViewBeerDesc = findViewById(R.id.textViewBeerDesc);
         imageViewBeer = findViewById(R.id.imageViewBeer);
+/*
+        Ion.with(v.getContext())
+                .load("https://api.punkapi.com/v2/beers?beer_name=" + searchText.getText() + "&per_page=5")
+                .setLogging("ION_LOGS", Log.DEBUG)
+                .asString()
+                .setCallback(new FutureCallback<String>() {
+                    @Override
+                    public void onCompleted(Exception e, String result) {
+                        try {
+                            JSONArray beerJsonObject = new JSONArray(result);
+                            for (int i = 0; i < beerJsonObject.length(); i++) {
+                                beersList.add(beerJsonObject.getJSONObject(i).getString("name"));
+                                beersidList.add(beerJsonObject.getJSONObject(i).getInt("id"));
+                            }
+                            listView.setAdapter(beersAdapter);
+                        } catch (JSONException jsonException) {
+                            jsonException.printStackTrace();
+                        }
+
+                    }
+                });*/
+
     }
 }
--- app/src/main/java/vigier/android/beer_search/MainActivity.java
+++ app/src/main/java/vigier/android/beer_search/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         button.setText("Search");
 
         ArrayList<String> beersList = new ArrayList<>();
+        ArrayList<Integer> beersidList = new ArrayList<>();
         ArrayAdapter<String> beersAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, beersList);
 
         listView.setAdapter(beersAdapter);
@@ -52,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 beersList.clear();
+                beersidList.clear();
                 Ion.with(v.getContext())
                         .load("https://api.punkapi.com/v2/beers?beer_name=" + searchText.getText() + "&per_page=5")
                         .setLogging("ION_LOGS", Log.DEBUG)
@@ -63,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
                                     JSONArray beerJsonObject = new JSONArray(result);
                                     for (int i = 0; i < beerJsonObject.length(); i++) {
                                         beersList.add(beerJsonObject.getJSONObject(i).getString("name"));
+                                        beersidList.add(beerJsonObject.getJSONObject(i).getInt("id"));
                                     }
                                     listView.setAdapter(beersAdapter);
                                 } catch (JSONException jsonException) {
@@ -78,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent beerInfoIntent = new Intent(MainActivity.this, BeerInfoActivity.class);
-                beerInfoIntent.putExtra("id", position);
+                beerInfoIntent.putExtra("id", beersidList.get(position));
                 startActivity(beerInfoIntent);
             }
         });
