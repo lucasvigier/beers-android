0e8eab90eec85d0ed3ef2c0268368c9203a62e36
E205827A <gwendal.faure@etu.univ-nantes.fr>
2021-03-27 22:20:30 +0100

    The End of History

--- app/src/main/java/vigier/android/beer_search/BeerInfoActivity.java
+++ app/src/main/java/vigier/android/beer_search/BeerInfoActivity.java
@@ -4,40 +4,70 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class BeerInfoActivity extends AppCompatActivity {
 
-    TextView textViewBeerInfo;
-    TextView textViewBeerAttributes;
-    TextView textViewBeerDesc;
+    TextView textViewBeerName;
     ImageView imageViewBeer;
+    TextView textViewBeerAttributeFirstBrew;
+    TextView textViewBeerDesc;
+    TextView textViewBeerAttributesSRM;
+    TextView textViewBeerAttributesPH;
+    TextView textViewBeerAttributesABV;
+    TextView textViewBeerAttributesSubTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beer_info);
 
-        textViewBeerInfo = findViewById(R.id.textViewBeerName);
-        textViewBeerAttributes = findViewById(R.id.textViewBeerAttributeFirstBrew);
+        textViewBeerName = findViewById(R.id.textViewBeerName);
+        textViewBeerAttributeFirstBrew = findViewById(R.id.textViewBeerAttributeFirstBrew);
         textViewBeerDesc = findViewById(R.id.textViewBeerDesc);
         imageViewBeer = findViewById(R.id.imageViewBeer);
+        textViewBeerAttributesSRM = findViewById(R.id.textViewBeerAttributesSRM);
+        textViewBeerAttributesPH = findViewById(R.id.textViewBeerAttributesPH);
+        textViewBeerAttributesABV = findViewById(R.id.textViewBeerAttributesABV);
+        textViewBeerAttributesSubTitle = findViewById(R.id.textViewBeerAttributesSubTitle);
 
         Bundle extras = getIntent().getExtras();
 
         int id = extras.getInt("id");
+        System.out.println(id);
 
         Ion.with(super.getBaseContext())
-                .load("https://api.punkapi.com/v2/beers" + id)
+                .load(("https://api.punkapi.com/v2/beers/" + id))
                 .setLogging("ION_LOGS", Log.DEBUG)
                 .asString()
                 .setCallback(new FutureCallback<String>() {
                     @Override
                     public void onCompleted(Exception e, String result) {
+                        try {
+                            JSONArray beerJsonObject = new JSONArray(result);
+                            textViewBeerDesc.setText(beerJsonObject.getJSONObject(0).getString("description"));
+                            textViewBeerName.setText(beerJsonObject.getJSONObject(0).getString("name"));
+                            Picasso.get().load(beerJsonObject.getJSONObject(0).getString("image_url")).into(imageViewBeer);
+                            textViewBeerAttributesSRM.setText(beerJsonObject.getJSONObject(0).getString("srm"));
+                            textViewBeerAttributesPH.setText(beerJsonObject.getJSONObject(0).getString("ph"));
+                            textViewBeerAttributesABV.setText(beerJsonObject.getJSONObject(0).getString("abv"));
+                            textViewBeerAttributesSubTitle.setText(beerJsonObject.getJSONObject(0).getString("tagline"));
+                            textViewBeerAttributeFirstBrew.setText(beerJsonObject.getJSONObject(0).getString("first_brewed"));
+
+                        } catch (JSONException jsonException) {
+                            jsonException.printStackTrace();
+                        }
+
 
                     }
                 });
